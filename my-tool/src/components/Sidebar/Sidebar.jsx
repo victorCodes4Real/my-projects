0a182bb999980018c4364d830982461353b3dd88
victorCodes4Real <achilonuvictor2014@gmail.com>
2024-08-02 14:27:49 +0100

    my deploy commit

---
+++ my-tool/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Sidebar.css' 
+import {assets} from '../../assets/assets'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+    <div className="top">
+ <img className='menu' src={assets.menu_icon} alt="" />
+ <div className="new-chat">
+    <img src={assets.plus_icon} alt="" />
+    <p>New Chat</p>
+ </div>
+ <div className="recent">
+    <p className="recent-title">Recent</p>
+    <div className="recent-entry">
+        <img src={assets.message_icon} alt="" />
+        <p></p>
+    </div>
+    </div>
+    </div>
+    <div className="bottom">
+
+    </div>
+    </div>
+  )
+}
+
+export default Sidebar
